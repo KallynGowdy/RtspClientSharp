56ed9a27021b9926bd3cc3a1b2c7ed6b0cc04432
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-11-12 16:48:45 -0500

    Don't wait for iframes after a reset
    - This can cause some issues with cameras that send a lot of P frames.

--- RtspClientSharp/MediaParsers/H264Parser.cs
+++ RtspClientSharp/MediaParsers/H264Parser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -124,7 +124,7 @@ namespace RtspClientSharp.MediaParsers
         {
             _frameStream.Position = 0;
             _sliceType = -1;
-            _waitForIFrame = true;
+            // _waitForIFrame = true;
         }
 
         private void SlicerOnNalUnitFound(ArraySegment<byte> byteSegment)
